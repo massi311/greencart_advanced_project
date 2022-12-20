4f832fa17a658381afd47bd62591c57f8dfe494b
matrix <masouda311@gmail.com>
2022-12-20 10:35:50 +0330

    changes

--- profiles_project/wsgi.py
+++ profiles_project/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for greencart_advanced_project project.
+WSGI config for profiles_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greencart_advanced_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profiles_project.settings')
 
 application = get_wsgi_application()
